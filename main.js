d1c4f739131725a384f2ac8efef269e7a4250b4f
Davina <36658212+Davina-T@users.noreply.github.com>
2018-11-26 23:45:15 +1000

    added icons and alert

--- main.js
+++ main.js
@@ -15,21 +15,25 @@ function addItem() {
     const userInput = document.getElementById("userInput");
     const toDoList = document.getElementById("itemsList");
 
-    // Create list item
-    const listItem = document.createElement("li");
+    if (userInput.value !== "") {
+        // Create list item
+        const listItem = document.createElement("li");
 
-    // Add input value to list item
-    listItem.innerText = userInput.value;
+        // Add input value to list item
+        listItem.innerHTML = `${userInput.value}<i class="fas fa-times"></i><i class="fas fa-check"></i>`;
 
-    // Append child
-    toDoList.appendChild(listItem);
+        // Append child
+        toDoList.appendChild(listItem);
 
-    // Local Storage
-    const itemArray = JSON.parse(localStorage.getItem("ListItems"));
-    itemArray.push(userInput.value);
-    localStorage.setItem("ListItems", JSON.stringify(itemArray));
+        // Local Storage
+        const itemArray = JSON.parse(localStorage.getItem("ListItems"));
+        itemArray.push(userInput.value);
+        localStorage.setItem("ListItems", JSON.stringify(itemArray));
 
-    userInput.value = "";
+        userInput.value = "";
+    } else {
+        alert("Please enter something");
+    }
 }
 
 function createLocalArray() {
@@ -43,7 +47,7 @@ function createLocalArray() {
 
         for (const item of itemArray) {
             const listItem = document.createElement("li");
-            listItem.innerText = item;
+            listItem.innerHTML = `${item}<i class="fas fa-times"></i><i class="fas fa-check"></i>`;
             toDoList.appendChild(listItem);
         }
     }
