6dbc62d0a9fe11c9837ef880b15e9871dd7910b7
Davina <36658212+Davina-T@users.noreply.github.com>
2018-11-26 23:26:15 +1000

    Added localstorage, userinput and list

--- main.js
+++ main.js
@@ -2,10 +2,49 @@ function removeCheckbox() {
     var checkBox = document.getElementById("myCheck");
 
     if (checkBox.checked == false) {
-        removeElm(checkBox);
+        removeElement(checkBox);
     }
 }
 
-function removeElm(element) {
+function removeElement(element) {
     element.parentElement.removeChild(element);
+}
+
+function addItem() {
+    // Get text input
+    const userInput = document.getElementById("userInput");
+    const toDoList = document.getElementById("itemsList");
+
+    // Create list item
+    const listItem = document.createElement("li");
+
+    // Add input value to list item
+    listItem.innerText = userInput.value;
+
+    // Append child
+    toDoList.appendChild(listItem);
+
+    // Local Storage
+    const itemArray = JSON.parse(localStorage.getItem("ListItems"));
+    itemArray.push(userInput.value);
+    localStorage.setItem("ListItems", JSON.stringify(itemArray));
+
+    userInput.value = "";
+}
+
+function createLocalArray() {
+    const toDoList = document.getElementById("itemsList");
+
+    if (localStorage.getItem("ListItems") === null) {
+        const Items = [];
+        localStorage.setItem("ListItems", JSON.stringify(Items));
+    } else {
+        const itemArray = JSON.parse(localStorage.getItem("ListItems"));
+
+        for (const item of itemArray) {
+            const listItem = document.createElement("li");
+            listItem.innerText = item;
+            toDoList.appendChild(listItem);
+        }
+    }
 }
